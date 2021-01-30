19ff74485d1fb35803cba6334eaf32ea16733959
Wilson Tseng <p780729@hotmail.com>
2021-01-30 14:01:59 +0800

    Finish U116 adminRestaurantForm

---
+++ src/views/AdminRestaurantNew.vue
@@ -0,0 +1,31 @@
+<template>
+ <div class="container py-5">
+    <!-- 餐廳表單 AdminRestaurantForm -->
+    <AdminRestaurantForm />
+  </div>
+</template>
+
+<script>
+import AdminRestaurantForm from './../components/AdminRestaurantForm'
+
+export default {
+  components: {
+    AdminRestaurantForm
+  },
+
+  data () {
+    return {
+      restaurant: {
+        name: '',
+        categoryId: '',
+        tel: '',
+        address: '',
+        description: '',
+        image: '',
+        openingHours: ''
+      },
+      categories: []
+    }
+  }
+}
+</script>

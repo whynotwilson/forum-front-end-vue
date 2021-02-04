2736eec41becb2875cc3293d41303b34b11f06c4
Wilson Tseng <p780729@hotmail.com>
2021-02-05 00:09:06 +0800

    Finish U132 Serial  usersTop API

--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -4,116 +4,54 @@
     <h1 class="mt-5">美食達人</h1>
     <hr />
     <div class="row text-center">
-      <FoodGuru v-for="user in usersTop" :key="user.id" :food-guru="user" />
+      <div class="col-3" v-for="user in usersTop" :key="user.id">
+        <a href="#">
+          <img :src="user.image" width="140px" height="140px" />
+        </a>
+        <router-link
+          :to="{
+            name: 'user-profile',
+            params: {
+              id: user.id,
+            },
+          }"
+        >
+          <h2>{{ user.name }}</h2>
+        </router-link>
+        <span class="badge badge-secondary"
+          >追蹤人數：{{ user.FollowerCount }}</span
+        >
+        <p class="mt-3">
+          <button
+            type="button"
+            class="btn btn-danger"
+            v-if="user.isFollowed"
+            @click.stop.prevent="deleteFollowing(user.id)"
+          >
+            取消追蹤
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            @click.stop.prevent="addFollowing(user.id)"
+            v-else
+          >
+            追蹤
+          </button>
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import NavTabs from "./../components/NavTabs";
-import FoodGuru from "./../components/FoodGuru";
-
-const dummyData = {
-  usersTop: [
-    {
-      id: 1,
-      name: "foodGuru1",
-      email: "root@example.com",
-      password: "$2a$10$jBS/Y4.hceDXkEC5y9ZGne81Y7i5wNwNcy6wAKjNdBykCzlEfWmLm",
-      isAdmin: true,
-      image: "https://i.imgur.com/3keAGHT.jpeg",
-      createdAt: "2020-12-15T06:35:43.000Z",
-      updatedAt: "2021-01-14T16:20:50.000Z",
-      Followers: [
-        {
-          id: 1,
-          name: "roo00t",
-          email: "root@example.com",
-          password:
-            "$2a$10$jBS/Y4.hceDXkEC5y9ZGne81Y7i5wNwNcy6wAKjNdBykCzlEfWmLm",
-          isAdmin: true,
-          image: "https://i.imgur.com/3keAGHT.jpeg",
-          createdAt: "2020-12-15T06:35:43.000Z",
-          updatedAt: "2021-01-14T16:20:50.000Z",
-          Followship: {
-            followerId: 1,
-            followingId: 1,
-            createdAt: "2021-01-05T17:22:26.000Z",
-            updatedAt: "2021-01-05T17:22:26.000Z",
-          },
-        },
-        {
-          id: 2,
-          name: "name2",
-          email: "user1@example.com",
-          password:
-            "$2a$10$m11qLlDOol1b3XCa393Bwe.hW4mt/6DS.mUsgFtati5LW4BbX81EG",
-          isAdmin: false,
-          image: "https://i.imgur.com/PhcKzNf.jpeg",
-          createdAt: "2020-12-15T06:35:43.000Z",
-          updatedAt: "2021-01-15T17:07:09.000Z",
-          Followship: {
-            followerId: 2,
-            followingId: 1,
-            createdAt: "2021-01-12T19:03:03.000Z",
-            updatedAt: "2021-01-12T19:03:03.000Z",
-          },
-        },
-      ],
-      FollowerCount: 2,
-      isFollowed: true,
-    },
-    {
-      id: 2,
-      name: "foodGuru2",
-      email: "user1@example.com",
-      password: "$2a$10$m11qLlDOol1b3XCa393Bwe.hW4mt/6DS.mUsgFtati5LW4BbX81EG",
-      isAdmin: false,
-      image: "https://i.imgur.com/PhcKzNf.jpeg",
-      createdAt: "2020-12-15T06:35:43.000Z",
-      updatedAt: "2021-01-15T17:07:09.000Z",
-      Followers: [],
-      FollowerCount: 0,
-      isFollowed: false,
-    },
-    {
-      id: 3,
-      name: "foodGuru3",
-      email: "user2@example.com",
-      password: "$2a$10$IgMneSD6HZiHt0C6we./cOPyq70YhAWNZEqC4YTtJHK8ejgS1J/3q",
-      isAdmin: false,
-      image: null,
-      createdAt: "2020-12-15T06:35:43.000Z",
-      updatedAt: "2020-12-15T06:35:43.000Z",
-      Followers: [
-        {
-          id: 2,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$m11qLlDOol1b3XCa393Bwe.hW4mt/6DS.mUsgFtati5LW4BbX81EG",
-          isAdmin: false,
-          image: "https://i.imgur.com/PhcKzNf.jpeg",
-          createdAt: "2020-12-15T06:35:43.000Z",
-          updatedAt: "2021-01-15T17:07:09.000Z",
-          Followship: {
-            followerId: 2,
-            followingId: 3,
-            createdAt: "2020-12-13T03:37:29.000Z",
-            updatedAt: "2020-12-13T03:37:29.000Z",
-          },
-        },
-      ],
-      FollowerCount: 1,
-      isFollowed: false,
-    },
-  ],
-};
+import usersAPI from "./../apis/users";
+import { Toast } from "./../utils/helpers";
 
 export default {
   components: {
     NavTabs,
-    FoodGuru,
   },
 
   data() {
@@ -123,12 +61,74 @@ export default {
   },
 
   created() {
-    this.fetchFoodGuru();
+    this.fetchTopUsers();
   },
 
   methods: {
-    fetchFoodGuru() {
-      this.usersTop = dummyData.usersTop;
+    async fetchTopUsers() {
+      try {
+        const { data } = await usersAPI.getTopUsers();
+        // console.log(data);
+        this.usersTop = data.users;
+      } catch (error) {
+        console.log(error);
+        Toast.fire({
+          icon: "error",
+          title: "無法取得美食達人，請稍後再試",
+        });
+      }
+    },
+
+    async addFollowing(userId) {
+      try {
+        const { data } = await usersAPI.addFollowing({ userId });
+        if (data.status !== "success") {
+          throw new Error(data.message);
+        }
+
+        this.usersTop = this.usersTop.map((user) => {
+          if (user.id !== userId) {
+            return user;
+          } else {
+            return {
+              ...user,
+              FollowerCount: user.FollowerCount + 1,
+              isFollowed: true,
+            };
+          }
+        });
+      } catch (error) {
+        Toast.fire({
+          icon: "error",
+          title: "無法加入追蹤，請稍後再試",
+        });
+      }
+    },
+
+    async deleteFollowing(userId) {
+      try {
+        const { data } = await usersAPI.deleteFollowing({ userId });
+        if (data.status !== "success") {
+          throw new Error(data.message);
+        }
+
+        this.usersTop = this.usersTop.map((user) => {
+          if (user.id !== userId) {
+            return user;
+          } else {
+            return {
+              ...user,
+              FollowerCount: user.FollowerCount - 1,
+              isFollowed: false,
+            };
+          }
+        });
+      } catch (error) {
+        Toast.fire({
+          icon: "error",
+          title: "無法加入追蹤，請稍後再試",
+        });
+      }
     },
   },
 };
